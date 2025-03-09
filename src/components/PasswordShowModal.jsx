e6540cc1a01a15a90ea120dd0d48b2ceddf4613e
EnsEtymz <etyimezenes8@gmail.com>
2025-03-09 14:37:20 +0300

    pagination eklendi password modal oluşturuldu

---
+++ src/components/PasswordShowModal.jsx
@@ -0,0 +1,125 @@
+import { Copy, Check } from "lucide-react";  // Check ikonu ekleniyor
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import useAuthStore from "@/app/authStore";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+export function PasswordShowModal({
+  showPassword,
+  setShowPassword,
+  selectedPassword,
+}) {
+  const token = useAuthStore((state) => state.token);
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const [password, setPassword] = useState();
+  const [copied, setCopied] = useState(false);  // Kopyalandı durumu için state
+
+  useEffect(() => {
+    if (!selectedPassword || !token) return; // Token veya seçili şifre yoksa durdur
+
+    const getPasswords = async () => {
+      try {
+        const response = await fetch(
+          `${baseUrl}/password-generator/${selectedPassword.id}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          toast.error("Passwords method is not defined!", {
+            style: { background: "#000", color: "#fff" },
+          });
+          return;
+        }
+
+        const res = await response.json();
+        if (res.is_success && res.data) {
+          setPassword(res.data.pass);
+          console.log("Password:", res.data);
+        } else {
+          toast.error("Passwords is not defined!", {
+            style: { background: "#000", color: "#fff" },
+          });
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        toast.error("An error occurred!", {
+          style: { background: "#000", color: "#fff" },
+        });
+      }
+    };
+
+    getPasswords();
+  }, [selectedPassword, token]);
+
+  const handleCopy = () => {
+    // Kopyalama işlemi
+    navigator.clipboard.writeText(password)
+      .then(() => {
+        setCopied(true);  // Kopyalandı durumunu değiştir
+        toast.success("Password copied successfully!", {
+          style: {
+            background: "#34c75a",
+            color: "#fff",
+          },
+        });
+        // 2 saniye sonra ikonu tekrar eski haline döndür
+        setTimeout(() => setCopied(false), 2000);
+      })
+      .catch((error) => {
+        toast.error("Failed to copy password!", {
+          style: {
+            background: "#000",
+            color: "#fff",
+          },
+        });
+      });
+  };
+
+  return (
+    selectedPassword ? (  
+      <Dialog open={showPassword} onOpenChange={()=>{setShowPassword(); setPassword(""); setCopied(false);}}>
+        <DialogContent className="sm:max-w-md rounded-md">
+          <DialogHeader>
+            <DialogTitle>{selectedPassword.name}</DialogTitle>
+          </DialogHeader>
+          <div className="flex items-center space-x-2">
+            <div className="grid flex-1 gap-2">
+              <Label htmlFor="link" className="sr-only">
+                Link
+              </Label>
+              <Input id="link" value={password} readOnly />
+            </div>
+            <Button
+              type="button"
+              size="sm"
+              className="px-3 bg-[#34c75a] hover:bg-[#2aa24a] transition duration-200"
+              onClick={handleCopy} // Kopyalama işlemini tetikle
+            >
+              <span className="sr-only">Copy</span>
+              {copied ? <Check /> : <Copy />}  {/* Eğer kopyalandıysa Check ikonu, değilse Copy ikonu */}
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    ) : null // Eğer seçili şifre yoksa modalı gösterme   
+  );
+  
+}
--- src/components/SavedPasswordList.jsx
+++ src/components/SavedPasswordList.jsx
@@ -1,161 +1,79 @@
 "use client";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { CardContent } from "./ui/card";
 import Image from "next/image";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import useAuthStore from "@/app/authStore";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+import { Eye, EyeOff } from "lucide-react";
+import { PasswordShowModal } from "./PasswordShowModal";
 
 export function SavedPasswordList() {
   const token = useAuthStore((state) => state.token);
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
   const [passwords, setPasswords] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [showPassword, setShowPassword] = useState(false);
+  const [selectedPassword, setSelectedPassword] = useState(null);
 
-  const getPasswords = async () => {
+  const getPasswords = async (page = 1) => {
     try {
-      const response = await fetch(`${baseUrl}/password-generator/query`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await fetch(
+        `${baseUrl}/password-generator/query?page=${page}&size=3`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       if (!response.ok) {
         toast.error("Passwords method is not defined!", {
-          style: {
-            background: "#000",
-            color: "#fff",
-          },
+          style: { background: "#000", color: "#fff" },
         });
         return;
       }
 
       const res = await response.json();
       if (res.is_success && res.data) {
-        setPasswords(res.data?.items);
+        setPasswords(res.data.items);
+        setCurrentPage(res.data.current_page);
+        setTotalPages(res.data.total_pages);
       } else {
         toast.error("Passwords is not defined!", {
-          style: {
-            background: "#000",
-            color: "#fff",
-          },
+          style: { background: "#000", color: "#fff" },
         });
       }
     } catch (error) {
       console.error("Error:", error);
       toast.error("An error occurred!", {
-        style: {
-          background: "#000",
-          color: "#fff",
-        },
+        style: { background: "#000", color: "#fff" },
       });
     }
   };
 
-  const getOnePassword = async () => {
-    try {
-      const id = "Oy6vESInUCptJjh9mn7zvA==";
-      const response = await fetch(`${baseUrl}/password-generator/${id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (!response.ok) {
-        toast.error("Passwords method is not defined!", {
-          style: {
-            background: "#000",
-            color: "#fff",
-          },
-        });
-        return;
-      }
-
-      const res = await response.json();
-      if (res.is_success && res.data) {
-      } else {
-        toast.error("Passwords is not defined!", {
-          style: {
-            background: "#000",
-            color: "#fff",
-          },
-        });
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      toast.error("An error occurred!", {
-        style: {
-          background: "#000",
-          color: "#fff",
-        },
-      });
-    }
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(date);
   };
 
   useEffect(() => {
-    getPasswords();
-   // getOnePassword();
-  }, []);
+    if (!token) return; // Token yoksa işlemi durdur
+    getPasswords(currentPage);
+  }, [currentPage, token]); // currentPage veya token değiştiğinde çalıştır
+
   return (
     <CardContent className="flex flex-col gap-6 w-full lg:w-3/5 bg-white rounded-md">
-      <div className="relative  bg-white flex flex-col items-center justify-center md:flex">
+      <div className="relative bg-white flex flex-col items-center justify-center">
         <Image
           src="/logo.png"
           alt="Image"
@@ -170,112 +88,96 @@ export function SavedPasswordList() {
           <h3 className="text-lg font-semibold text-slate-800">My Passwords</h3>
           <p className="text-slate-500">A list of your saved passwords.</p>
         </div>
-        <div className="ml-3">
-          <div className="w-full max-w-sm min-w-[200px] relative">
-            <div className="relative">
-              <input
-                className="bg-white w-full pr-11 h-10 pl-3 py-2 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded transition duration-200 ease focus:outline-none focus:border-slate-400 hover:border-slate-400  focus:shadow-md"
-                placeholder="Search for passwords..."
-              />
+      </div>
+
+      {passwords.length > 0 ? (
+        <>
+          <table className="w-full text-left table-auto min-w-max">
+            <thead>
+              <tr>
+                <th className="p-4 border-b border-slate-200 bg-slate-50 text-sm font-normal text-slate-500">
+                  Name
+                </th>
+                <th className="p-4 border-b border-slate-200 bg-slate-50 text-sm font-normal text-slate-500">
+                  Hint
+                </th>
+                <th className="p-4 border-b border-slate-200 bg-slate-50 text-sm font-normal text-slate-500">
+                  Password
+                </th>
+                <th className="p-4 border-b border-slate-200 bg-slate-50 text-sm font-normal text-slate-500">
+                  CreatedOn
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {passwords.map((password) => (
+                <tr key={password.id}>
+                  <td className="p-4 text-sm border-b border-slate-200 font-semibold">
+                    {password.name}
+                  </td>
+                  <td className="p-4 text-sm border-b border-slate-200">
+                    {password.hint}
+                  </td>
+                  <td className="p-4 text-sm border-b border-slate-200 flex gap-1">
+                    ********
+                    <Eye
+                      className="cursor-pointer"
+                      size={14}
+                      onClick={() => {
+                        setSelectedPassword(password);
+                        setShowPassword(true);
+                      }}
+                    />
+                  </td>
+                  <td className="p-4 text-sm border-b border-slate-200">
+                    {formatDate(password.created_on)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {/* Sayfalama Butonları */}
+
+          <div className="flex justify-between items-center px-4 py-3">
+            <div className="text-sm text-slate-500">
+              Page{" "}
+              <b>
+                {currentPage} of {totalPages}
+              </b>
+            </div>
+            <div className="flex space-x-1">
               <button
-                className="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center bg-white rounded "
-                type="button"
+                onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                disabled={currentPage === 1}
+                className={`px-3 py-1.5 min-w-9 text-sm font-normal text-slate-500  border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease ${
+                  currentPage === 1 ? "bg-slate-50" : "bg-white"
+                }`}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="3"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-slate-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                  />
-                </svg>
+                Prev
+              </button>
+              <button
+                onClick={() =>
+                  setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+                }
+                disabled={currentPage === totalPages}
+                className={`px-3 py-1.5 min-w-9  text-sm font-normal text-slate-500  border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease ${
+                  currentPage === totalPages ? "bg-slate-50" : "bg-white "
+                }`}
+              >
+                Next
               </button>
             </div>
           </div>
-        </div>
-      </div>
-      {passwords.length > 0 ? (
-      <div className="relative flex flex-col w-full h-full text-gray-700 bg-white rounded-lg bg-clip-border">
-        <table className="w-full text-left table-auto min-w-max">
-          <thead>
-            <tr>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className="text-sm font-normal leading-none text-slate-500">
-                  Name
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className="text-sm font-normal leading-none text-slate-500">
-                  Hint
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className="text-sm font-normal leading-none text-slate-500">
-                  Password
-                </p>
-              </th>
-              <th className="p-4 border-b border-slate-200 bg-slate-50">
-                <p className="text-sm font-normal leading-none text-slate-500">
-                  CreatedOn
-                </p>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {passwords.map((item, key) => (
-              <tr key={key} className="hover:bg-slate-50 border-b border-slate-200">
-                <td className="p-4 py-5">
-                  <p className="block font-semibold text-sm text-slate-800">
-                    {item.name}
-                  </p>
-                </td>
-                <td className="p-4 py-5">
-                  <p className="text-sm text-slate-500">{item.hint}</p>
-                </td>
-                <td className="p-4 py-5">
-                  <p className="text-sm text-slate-500">********</p>
-                </td>
-                <td className="p-4 py-5">
-                  <p className="text-sm text-slate-500">{item.created_on}</p>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-
-        <div className="flex justify-between items-center px-4 py-3">
-          <div className="text-sm text-slate-500">
-            Showing <b>1-5</b> of 45
-          </div>
-          <div className="flex space-x-1">
-            <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-              Prev
-            </button>
-            <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
-              1
-            </button>
-            <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-              2
-            </button>
-            <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-              3
-            </button>
-            <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-              Next
-            </button>
-          </div>
-        </div>
-      </div> ) : (
-        <div className="flex justify-center items-center h-40">
-          <p className="text-slate-500">No passwords found!</p>
-        </div>
+        </>
+      ) : (
+        <p className="text-center text-slate-500">No passwords found.</p>
       )}
+      <PasswordShowModal
+        showPassword={showPassword}
+        setShowPassword={setShowPassword}
+        selectedPassword={selectedPassword}
+      />
     </CardContent>
   );
 }
